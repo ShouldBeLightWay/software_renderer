ceed6b2c2c730753c1473c7757afc7895ce4d55c
Nickolay Vostrikov <nickolaivostrikov@yandex.ru>
2025-12-06 22:52:48 +0300

    Add .clang-format file and improve code formatting in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include <SDL3/SDL.h>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+int main( int argc, char *argv[] )
+{
     // Initialize SDL
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    if( !SDL_Init( SDL_INIT_VIDEO ) )
+    {
         std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
         return 1;
     }
 
     // Create window
-    SDL_Window* window = SDL_CreateWindow(
-        "Software Renderer",
-        800, 600,
-        SDL_WINDOW_RESIZABLE
-    );
+    SDL_Window *window = SDL_CreateWindow( "Software Renderer", 800, 600, SDL_WINDOW_RESIZABLE );
 
-    if (!window) {
+    if( !window )
+    {
         std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
@@ -25,19 +24,22 @@ int main(int argc, char* argv[]) {
     bool running = true;
     SDL_Event event;
 
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
+    while( running )
+    {
+        while( SDL_PollEvent( &event ) )
+        {
+            if( event.type == SDL_EVENT_QUIT )
+            {
                 running = false;
             }
         }
 
         // Keep the window responsive
-        SDL_Delay(16); // ~60 FPS
+        SDL_Delay( 16 ); // ~60 FPS
     }
 
     // Cleanup
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow( window );
     SDL_Quit();
 
     return 0;
