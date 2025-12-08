f38f5c60cdb70e26a277fdddc0f020b1a9fb1019
Nickolay Vostrikov <nickolaivostrikov@yandex.ru>
2025-12-08 04:56:36 +0300

    Add stages for vertex, rasterization, pixel shader, and output merger; introduce viewport struct

--- src/swrDevice.h
+++ src/swrDevice.h
@@ -57,6 +57,17 @@ namespace swr
         // Добавить другие форматы по мере необходимости
     };
 
+    // Порт вывода (viewport)
+    struct Viewport
+    {
+        int x;
+        int y;
+        int width;
+        int height;
+        float minDepth;
+        float maxDepth;
+    };
+
     // Устройство рендеринга
     class Device : public std::enable_shared_from_this<Device>
     {
@@ -90,6 +101,101 @@ namespace swr
             std::shared_ptr<Buffer> indexBuffer;
             PrimitiveTopology primitiveTopology;
         };
+
+        // VS Vertex Shader stage
+        class VSStage
+        {
+          public:
+            void setVertexShader( VertexShader shader );
+
+          private:
+            friend class Device;
+            VSStage( std::shared_ptr<Device> device ) : parentDevice( device )
+            {
+            }
+            std::shared_ptr<Device> parentDevice;
+            VertexShader vertexShader;
+        };
+
+        // RS (Rasterizer) stage
+        class RSStage
+        {
+          public:
+            // В будущем можно добавить настройки растеризации
+            void setViewport( const Viewport &viewport );
+            void setCullBackface( bool cull );
+            void setWireframe( bool wireframe );
+
+          private:
+            friend class Device;
+            RSStage( std::shared_ptr<Device> device ) : parentDevice( device )
+            {
+            }
+            std::shared_ptr<Device> parentDevice;
+            Viewport viewport;
+            bool cullBackface = false;
+            bool wireframe = false;
+        };
+
+        // PS Pixel Shader stage
+        class PSStage
+        {
+          public:
+            void setPixelShader( PixelShader shader );
+
+          private:
+            friend class Device;
+            PSStage( std::shared_ptr<Device> device ) : parentDevice( device )
+            {
+            }
+            std::shared_ptr<Device> parentDevice;
+            PixelShader pixelShader;
+        };
+
+        // OM Output Merger stage
+        class OMStage
+        {
+          public:
+            void setClearColor( const glm::vec4 &color );
+            glm::vec4 clearColor() const;
+            // В будущем можно добавить настройки слияния вывода
+          private:
+            friend class Device;
+            OMStage( std::shared_ptr<Device> device ) : parentDevice( device )
+            {
+            }
+            std::shared_ptr<Device> parentDevice;
+            glm::vec4 clearColorValue;
+        };
+
+        // Доступ к стадиям конвейера
+        IAStage &IA()
+        {
+            return iaStage;
+        }
+        VSStage &VS()
+        {
+            return vsStage;
+        }
+        RSStage &RS()
+        {
+            return rsStage;
+        }
+        PSStage &PS()
+        {
+            return psStage;
+        }
+        OMStage &OM()
+        {
+            return omStage;
+        }
+
+      private:
+        IAStage iaStage;
+        VSStage vsStage;
+        RSStage rsStage;
+        PSStage psStage;
+        OMStage omStage;
     };
 
 } // namespace swr
